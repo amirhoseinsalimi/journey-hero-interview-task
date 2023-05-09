c774a8083c58630a9979ef1d72e121756188d13f
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2023-05-09 22:26:00 +0330

    feat: add in-app back button

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <VAppBar color="primary" :elevation="2">
+      <template v-if="canClickBack" #prepend>
+        <VAppBarNavIcon
+          color="white"
+          icon="mdi-arrow-left"
+          @click="routeBack"
+        />
+      </template>
       <VContainer>
         <VAppBarTitle>
           {{ routeName }}
@@ -16,9 +23,13 @@
 
 <script setup lang="ts">
 import { computed } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 
 const route = useRoute()
+const router = useRouter()
 
 const routeName = computed(() => route.name)
+const canClickBack = computed(() => route.name !== 'Home')
+
+const routeBack = () => router.back()
 </script>
