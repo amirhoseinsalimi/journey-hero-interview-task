eadd5dc5a51ad86983bfc34485c66fd7a815297e
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2023-05-10 00:45:19 +0330

    feat(i18n): add internationalization support

---
+++ src/helpers/i18n.ts
@@ -0,0 +1,14 @@
+import type { I18nKeys } from '../i18n'
+import en from '../i18n/translations/en.json'
+
+const templateMatcher = /{{\s?([^{}\s]*)\s?}}/g
+
+export default (key: I18nKeys, args?: Record<string, unknown>) => {
+  return en[key].replace(templateMatcher, (substring, value) => {
+    if (args) {
+      value = args[value]
+    }
+
+    return value
+  })
+}
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,1 +1,2 @@
 export { default as getNowInMilliseconds } from './getNowInMilliseconds'
+export { default as i18n } from './i18n'
